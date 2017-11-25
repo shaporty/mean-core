4a5fd96cc9b086bc06d5e61af85da8af871ec39b
mehdi m salehi <shaporty@gmail.com>
2017-11-25 14:29:52 +0330

    socket io added

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose =require('mongoose');
+var mongoose = require('mongoose');
 
 var api = require('./routes/Api/api');
 var users_api = require('./routes/Api/users');
 
-var index =  require('./routes/index');
+var index = require('./routes/index');
 
 var app = express();
 
@@ -32,9 +32,9 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -42,24 +42,26 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send({
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.send({
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send({
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: {}
+    });
 });
+
+
 
 // mongoDB Connect
 mongoose.connect("mongodb://localhost/express-core");
---
+++ routes/sockets.js
@@ -0,0 +1,9 @@
+module.exports = function(socket) {
+    socket.on('user:join', function(data) {
+        console.log("join called");
+        //controllers.SiteController.welcome(socket, data);
+    });
+    socket.on('connect', function(data) {
+        //controllers.SiteController.aleyk(socket, data);
+    });
+}
